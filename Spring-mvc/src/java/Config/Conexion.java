87dd5e2cde0fa96f0e53c87b071d057c8cdcca11
frankfisher <frankcopser1163@gmail.com>
2019-05-03 12:27:51 -0500

    Se hizo la base de datos con la conexion

--- Spring-mvc/src/java/Config/Conexion.java
+++ Spring-mvc/src/java/Config/Conexion.java
@@ -5,10 +5,22 @@
  */
 package Config;
 
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 /**
  *
  * @author upgenia
  */
 public class Conexion {
-    
+
+    public DriverManagerDataSource Conectar(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/registro");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
 }
